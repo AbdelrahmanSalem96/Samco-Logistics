6c62f8e8998790b73a81a5f04c6e8c2355d1ea90
Abdelrahman <abdomsalem895@gmail.com>
2025-03-14 08:35:03 +0200

    add language button

--- js/main.js
+++ js/main.js
@@ -1,168 +1,202 @@
-// Fixed menue
-window.addEventListener("scroll", function () {
-  let fixedNav = document.querySelector(".top-bar");
-  let scrollPosition = window.scrollY;
-
-  if (scrollPosition > 100) {
-      fixedNav.style.display = "flex";
+// Toggle Language Button Text & Reload Page
+function toggleLanguage() {
+  const langBtn = document.getElementById("langToggle");
+  const currentLang = document.documentElement.lang;
+  // currentLang = "EN";
+  if (currentLang === "ar") {
+    document.documentElement.lang = "en";
+    localStorage.setItem("lang", "en");
+    langBtn.textContent = "عربي";
+    document
+      .getElementById("langToggle")
+      .addEventListener("click", function () {
+        updateNavigation("ar"); // Switch to Arabic
+      });
   } else {
-      fixedNav.style.display = "none";
+    document.documentElement.lang = "ar";
+    localStorage.setItem("lang", "ar"); // Save preference
+    langBtn.textContent = "عربي";
+    document
+      .getElementById("langToggle")
+      .addEventListener("click", function () {
+        updateNavigation("en"); // Switch to Arabic
+      });
   }
+
+  location.reload(); // Reload to apply changes
+}
+
+// Apply Saved Language on Page Load
+document.addEventListener("DOMContentLoaded", function () {
+  const savedLang = localStorage.getItem("lang") || "en";
+  document.documentElement.lang = savedLang;
+
+  const langBtn = document.getElementById("langToggle");
+  langBtn.textContent = savedLang === "en" ? "عربي" : "EN";
+
+  // updateNavigation(savedLang);
+  updateTextContent(savedLang);
+});
+
+// toggle menu
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("nav-toggle");
+  const navMenu = document.getElementById("nav-menu");
+
+  toggleButton.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
 });
 
 //  our service slider animation ---------------------
 document.addEventListener("DOMContentLoaded", function () {
-    new Swiper(".mySwiper" , {
-      slidesPerView: 3, // Show 3 slides at a time
-      spaceBetween: 30, // Space between slides
-      loop: true, // Enable infinite loop
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 4000, // Auto-slide every 3 seconds
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        1024: { slidesPerView: 3 }, // Desktop: 3 slides
-        768: { slidesPerView: 2 }, // Tablet: 2 slides
-        480: { slidesPerView: 1 }, // Mobile: 1 slide
-      },
+  new Swiper(".mySwiper", {
+    slidesPerView: 3, // Show 3 slides at a time
+    spaceBetween: 30, // Space between slides
+    loop: true, // Enable infinite loop
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 4000, // Auto-slide every 3 seconds
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 }, // Desktop: 3 slides
+      768: { slidesPerView: 2 }, // Tablet: 2 slides
+      480: { slidesPerView: 1 }, // Mobile: 1 slide
+    },
+  });
+});
+
+// testimonial slider
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".testimonial-slider", {
+    slidesPerView: 2,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+    },
+  });
+});
+
+//  slider animation ---------------------
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".portfolio-slider", {
+    slidesPerView: 3, // Show 3 slides at a time
+    spaceBetween: 15, // Space between slides
+    loop: true, // Enable infinite loop
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 4000, // Auto-slide every 3 seconds
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 }, // Desktop: 3 slides
+      768: { slidesPerView: 2 }, // Tablet: 2 slides
+      480: { slidesPerView: 1 }, // Mobile: 1 slide
+    },
+  });
+});
+
+// team slider
+//  slider animation ---------------------
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".team-swiper", {
+    slidesPerView: 4, // Show 3 slides at a time
+    spaceBetween: 45, // Space between slides
+    loop: true, // Enable infinite loop
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 40000000, // Auto-slide every 3 seconds
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 4 }, // Desktop: 3 slides
+      768: { slidesPerView: 2 }, // Tablet: 2 slides
+      480: { slidesPerView: 1 }, // Mobile: 1 slide
+    },
+  });
+});
+
+// Tap Menu -------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const tabPanels = document.querySelectorAll(".tab-panel");
+
+  tabButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Remove "active" class from all buttons
+      tabButtons.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
+
+      // Hide all tab panels and show the selected one
+      tabPanels.forEach((panel) => panel.classList.remove("active"));
+      document.getElementById(this.dataset.tab).classList.add("active");
     });
   });
-  
-  // testimonial slider
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    new Swiper(".testimonial-slider", {
-        slidesPerView: 2,
-        spaceBetween: 0,
-        loop: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: { slidesPerView: 1 },
-            768: { slidesPerView: 2 },
-        }
-    });
+
+  document.querySelector(".tab-btn .active")?.click();
+});
+
+//  blog section --------------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".blog-swiper", {
+    slidesPerView: 3, // Show 3 slides at a time
+    spaceBetween: 30, // Space between slides
+    loop: true, // Enable infinite loop
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 40000000, // Auto-slide every 3 seconds
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1024: { slidesPerView: 3 }, // Desktop: 3 slides
+      768: { slidesPerView: 2 }, // Tablet: 2 slides
+      480: { slidesPerView: 1 }, // Mobile: 1 slide
+    },
   });
-  
-  //  slider animation ---------------------
-  document.addEventListener("DOMContentLoaded", function () {
-    new Swiper(".portfolio-slider", {
-      slidesPerView: 3, // Show 3 slides at a time
-      spaceBetween: 15, // Space between slides
-      loop: true, // Enable infinite loop
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 4000, // Auto-slide every 3 seconds
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        1024: { slidesPerView: 3 }, // Desktop: 3 slides
-        768: { slidesPerView: 2 }, // Tablet: 2 slides
-        480: { slidesPerView: 1 }, // Mobile: 1 slide
-      },
-    });
-  });
-  
-  
-  // team slider
-  //  slider animation ---------------------
-  document.addEventListener("DOMContentLoaded", function () {
-    new Swiper(".team-swiper" , {
-      slidesPerView: 4, // Show 3 slides at a time
-      spaceBetween: 45, // Space between slides
-      loop: true, // Enable infinite loop
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 40000000, // Auto-slide every 3 seconds
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        1024: { slidesPerView: 4 }, // Desktop: 3 slides
-        768: { slidesPerView: 2 }, // Tablet: 2 slides
-        480: { slidesPerView: 1 }, // Mobile: 1 slide
-      },
-    });
-  });
-  
-  
-  // Tap Menu -------------------------
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const tabButtons = document.querySelectorAll(".tab-btn");
-    const tabPanels = document.querySelectorAll(".tab-panel");
-  
-    tabButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            // Remove active class from all buttons
-            tabButtons.forEach(btn => btn.classList.remove("active"));
-            this.classList.add("active");
-  
-            // Hide all panels and show the selected one
-            tabPanels.forEach(panel => panel.classList.remove("active"));
-            document.getElementById(this.dataset.tab).classList.add("active");
-        });
-    });
-  });
-  
-  //  blog section --------------------------
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    new Swiper(".blog-swiper" , {
-      slidesPerView: 3, // Show 3 slides at a time
-      spaceBetween: 30, // Space between slides
-      loop: true, // Enable infinite loop
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 40000000, // Auto-slide every 3 seconds
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        1024: { slidesPerView: 3 }, // Desktop: 3 slides
-        768: { slidesPerView: 2 }, // Tablet: 2 slides
-        480: { slidesPerView: 1 }, // Mobile: 1 slide
-      },
-    });
-  });
-  
-  
-  
-  
+});
